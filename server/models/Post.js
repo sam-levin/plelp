2e4bc47a1005b9bc59bd57b88207a3af8598fa5a
sauln179 <saulnavarro179@gmail.com>
2022-06-30 19:09:59 -0700

    Models

---
+++ server/models/Post.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+
+const postSchema = new Schema ({
+    user: [{type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'}],
+    title: {
+         type: String,
+         required: true
+     },
+     description: {
+         type: String,
+         required: true
+     },
+
+     createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+     }
+
+})
+
+
+const Post = model('Post', postSchema);
+
+module.exports = Post;
