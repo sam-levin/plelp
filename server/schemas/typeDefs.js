164e21b7e1740852da0dd0831e07e6c3bed1ceb3
sam-levin <sam.ky.levin@gmail.com>
2022-07-01 18:13:50 -0700

    updating typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+
+const typeDefs = gql`
+type User {
+  _id: ID
+  username: String
+  email: String
+  posts: [Post]
+}
+
+type Post {
+  _id: ID
+  postText: String
+  createdAt: String
+  username: String
+  replyCount: Int
+  reply: [Reply]
+}
+
+type Reply {
+  _id: ID
+  replyBody: String
+  createdAt: String
+  username: String
+}
+
+type Auth {
+  token: ID!
+  user: User
+}
+
+type Query {
+  me: User
+  users: [User]
+  user(username: String!): User
+  posts(username: String): [Post]
+  post(_id: ID!): Post
+}
+
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  addPost(postText: String!): Post
+  addReply(postId: ID!, reactionBody: String!): Post
+}
+`;
+
+module.exports = typeDefs;
