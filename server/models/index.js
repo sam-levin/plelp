606ca9e50d5f7f766e6e127e912038ef4c759a1e
sam-levin <sam.ky.levin@gmail.com>
2022-07-02 10:49:19 -0700

    small changes

--- server/models/index.js
+++ server/models/index.js
@@ -1,14 +1,7 @@
 const User = require('./User');
 const City = require('./City');
-<<<<<<< HEAD
-const Post = require('./Post');
-const Reply = require('./Reply');
-
-module.exports = { User, Post, City, Reply };
-=======
 const Location = require('./Location');
 const Post = require('./Post');
 const Reply = require('./City');
 
 module.exports = { User, City, Location, Post, Reply };
->>>>>>> feature/queries
