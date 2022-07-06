06c4a538aaea0193b9ee7d2a56b921e658714763
sauln179 <saulnavarro179@gmail.com>
2022-07-05 19:23:27 -0700

    Configure mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,25 +12,36 @@ mutation login($email: String!, $password: String!) {
   }`
 
   export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
+  mutation addUser(
+    $username: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      username: $firstName
+      email: $email
+      password: $password
+    ) {
       token
       user {
         _id
-        username
       }
     }
   }
 `;
 
 export const ADD_POST = gql`
-mutation addPost($description: String!)
-addPost(description: $description){
+mutation addPost(postText: String!)
+addPost(postText: $postText){
     _id
-    description
+    postText
     username
     title
     created_at
+    replyCount
+    replies{
+      _id
+    }
 
 
 }`
