aca34b0e30684926af1605f40f4e4d7bd6903a0c
sam-levin <sam.ky.levin@gmail.com>
2022-07-10 23:13:37 -0700

    changing up connection file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/mernshopping",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/mernshopping"
+    .then(connect => console.log('connected to mongodb..'))
+    .catch(e => console.log('could not connect to mongodb', e)),
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
